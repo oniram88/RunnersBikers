364fedff6dcd0b65eaa6ecbf35851908d07c1255
Marino Bonetti <marinobonetti@gmail.com>
2017-11-30 13:52:56 +0100

    commentati i sistemi di login strava e facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,12 +251,12 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :strava, Rails.application.secrets.strava_client_id,
-                  Rails.application.secrets.strava_api_key, scope: 'public'
-  config.omniauth :facebook, Rails.application.secrets.facebook_key,
-                  Rails.application.secrets.facebook_secret,
-                  scope: 'email,public_profile',
-                  info_fields: 'email,first_name,last_name'
+  # config.omniauth :strava, Rails.application.secrets.strava_client_id,
+  #                 Rails.application.secrets.strava_api_key, scope: 'public'
+  # config.omniauth :facebook, Rails.application.secrets.facebook_key,
+  #                 Rails.application.secrets.facebook_secret,
+  #                 scope: 'email,public_profile',
+  #                 info_fields: 'email,first_name,last_name'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
