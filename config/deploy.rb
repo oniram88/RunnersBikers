9f3431be681b984c81ed01f26628a2b16d803174
Marino Bonetti <marinobonetti@gmail.com>
2017-12-01 23:02:35 +0100

    disabilitazione secrets_enc

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/home/marinobonetti/hosting_mio/RunnerBikers"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml" ,"config/secrets.yml.key"#, "config/secrets.yml"
+append :linked_files, "config/database.yml" ,"config/secrets.yml"#, "config/secrets.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "node_modules"
