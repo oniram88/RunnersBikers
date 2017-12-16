668a8df89e580013ce3eb3e915a2104b81eb1674
Marino Bonetti <marinobonetti@gmail.com>
2017-12-16 16:49:28 +0100

    traduzioni per registrazione e ripristino emails

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -18,6 +18,6 @@ class RegistrationsController < Devise::RegistrationsController
   def configure_permitted_parameters
     # Permit the `subscribe_newsletter` parameter along with the other
     # sign up parameters.
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username,:first_name,:last_name])
   end
 end
