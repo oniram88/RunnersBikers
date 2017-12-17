0ae66da0a1fa17a7b74022093e55aebd1e8bbc32
Marino Bonetti <marinobonetti@gmail.com>
2017-12-17 21:49:39 +0100

    non eseguo se sono in test

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,8 @@
-Recaptcha.configure do |config|
-  config.site_key  = Rails.application.secrets.recaptcha[:site_key]
-  config.secret_key = Rails.application.secrets.recaptcha[:secret_key]
-  # Uncomment the following line if you are using a proxy server:
-  # config.proxy = 'http://myproxy.com.au:8080'
+unless Rails.env.test?
+  Recaptcha.configure do |config|
+    config.site_key = Rails.application.secrets.recaptcha[:site_key]
+    config.secret_key = Rails.application.secrets.recaptcha[:secret_key]
+    # Uncomment the following line if you are using a proxy server:
+    # config.proxy = 'http://myproxy.com.au:8080'
+  end
 end
