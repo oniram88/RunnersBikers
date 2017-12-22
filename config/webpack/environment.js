b1ea9a356f0d3c72b04690d7f09cbf242b85c70b
Marino Bonetti <marinobonetti@gmail.com>
2017-12-22 14:02:16 +0100

    correzione per deploy completo

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,16 +1,23 @@
-const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
+const {environment} = require('@rails/webpacker');
+const vue = require('./loaders/vue');
+const webpack = require('webpack');
 
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+environment.plugins.set('UglifyJs',
+  new UglifyJsPlugin()
+);
 
 environment.plugins.set(
-    'Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery',
-        Popper: ['popper.js', 'default']
-    })
-)
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery',
+    Popper: ['popper.js', 'default']
+  }),
+);
 
 
-module.exports = environment
+environment.loaders.append('vue', vue);
+module.exports = environment;
