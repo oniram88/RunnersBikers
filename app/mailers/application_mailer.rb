0e78ea953b46e468a59f7521d7cb0bec6d0f696c
Marino Bonetti <marinobonetti@gmail.com>
2017-12-22 19:12:04 +0100

    varie correzioni per indirizzo di posta e host usato nei mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'challanger@runnersbikers.it'
+  default from: 'challenger@runnersbikers.it'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'challange.runnersbikers.it', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'challenge.runnersbikers.it', port: 80 }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
