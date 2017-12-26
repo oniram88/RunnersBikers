ab44789a90908143dfce64fb9ad8dd0da16bdc45
Marino Bonetti <marinobonetti@gmail.com>
2017-12-26 10:53:12 +0100

    riporto modifiche per correzione performances

--- app/custom_types/pace_type.rb
+++ app/custom_types/pace_type.rb
@@ -21,14 +21,21 @@ class PaceType < ActiveRecord::Type::Value
     to_seconds(value)
   end
 
+  def self.serialize(value)
+    to_seconds(value)
+  end
+
   def self.reg_exp
     /\A(?<minuti>[0-5]?[0-9]):(?<secondi>([0-5][0-9]))\Z/
   end
 
-  private
   def to_seconds(value)
-    if value.is_a?(String) and value.match(self.class.reg_exp)
-      ris = value.match(self.class.reg_exp)
+    self.class.to_seconds(value)
+  end
+
+  def self.to_seconds(value)
+    if value.is_a?(String) and value.match(reg_exp)
+      ris = value.match(reg_exp)
       ris[:minuti].to_i * 60 + ris[:secondi].to_i
     else
       value
