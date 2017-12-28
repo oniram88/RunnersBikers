d1a0c743b74f8a294601178100a08eafbcf1f1a7
Marino Bonetti <marinobonetti@gmail.com>
2017-12-28 21:08:26 +0100

    correzione commento del check

--- app/models/user.rb
+++ app/models/user.rb
@@ -162,9 +162,9 @@ class User < ApplicationRecord
   end
 
   def check_max_registration
-    # if Time.now >= RunnersBikers::MAX_ISCRIZIONE
+    if Time.now >= RunnersBikers::MAX_ISCRIZIONE
     self.errors.add(:base, :max_iscrizione_superata, max_time: I18n.l(RunnersBikers::MAX_ISCRIZIONE, format: :short))
-    # end
+    end
   end
 
 end
