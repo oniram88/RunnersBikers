63854833789f3051f841271fa3d120f682ef756b
Marino Bonetti <marinobonetti@gmail.com>
2018-01-06 18:56:16 +0100

    correzione date durata challenger
    #26

--- config/initializers/runners_bikers.rb
+++ config/initializers/runners_bikers.rb
@@ -7,17 +7,17 @@ module RunnersBikers
   # Durata Sfida
   MATCH_DURATION = PROGRAMM_STATUS == 'test' ? 10.minutes : 5.days
 
-  # Tempo massimo inserimento Performance prima della fine della gara
-  MAX_PERFORMANCE_INSERT = Time.zone.parse('2018-02-12 00:00:00')
-
   # Tempo inizio inserimento Performance
-  MIN_PERFORMANCE_INSERT = PROGRAMM_STATUS == 'test' ? Time.now - 1.month : Time.zone.parse('2018-01-14 00:00:00')
+  MIN_PERFORMANCE_INSERT = PROGRAMM_STATUS == 'test' ? Time.now - 1.month : Time.zone.parse('2018-02-04 00:00:00')
+
+  # Tempo massimo inserimento Performance prima della fine della gara
+  MAX_PERFORMANCE_INSERT = Time.zone.parse('2018-03-05 00:00:00')
 
   # Tempo massimo iscrizioni
   MAX_ISCRIZIONE = PROGRAMM_STATUS == 'test' ? Time.now + 1.year : Time.zone.parse('2018-01-13 00:00:00')
 
   # Time to start challengers
-  TIME_TO_START_CHALLENGES = PROGRAMM_STATUS == 'test' ? Time.now - 1.day : Time.zone.parse('2018-01-26 00:00:00')
+  TIME_TO_START_CHALLENGES = PROGRAMM_STATUS == 'test' ? Time.now - 1.day : Time.zone.parse('2018-02-18 00:00:00')
 
   # Tempo massimo Inizio sfide
   MAX_TIME_FOR_START_CHALLENGES = PROGRAMM_STATUS == 'test' ? Time.now + 1.month : MAX_PERFORMANCE_INSERT - MATCH_DURATION - 2.hours
