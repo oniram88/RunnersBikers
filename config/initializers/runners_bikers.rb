a50f5eb560f4cb9938284dcfed1295522319f59d
Marino Bonetti <marinobonetti@gmail.com>
2018-01-06 22:40:42 +0100

    modifica dei limiti
    fix #26

--- config/initializers/runners_bikers.rb
+++ config/initializers/runners_bikers.rb
@@ -11,10 +11,10 @@ module RunnersBikers
   MIN_PERFORMANCE_INSERT = PROGRAMM_STATUS == 'test' ? Time.now - 1.month : Time.zone.parse('2018-02-04 00:00:00')
 
   # Tempo massimo inserimento Performance prima della fine della gara
-  MAX_PERFORMANCE_INSERT = Time.zone.parse('2018-03-05 00:00:00')
+  MAX_PERFORMANCE_INSERT = Time.zone.parse('2018-03-05 00:00:00') + 2.hours
 
   # Tempo massimo iscrizioni
-  MAX_ISCRIZIONE = PROGRAMM_STATUS == 'test' ? Time.now + 1.year : Time.zone.parse('2018-01-13 00:00:00')
+  MAX_ISCRIZIONE = PROGRAMM_STATUS == 'test' ? Time.now + 1.year : MIN_PERFORMANCE_INSERT #Time.zone.parse('2018-01-13 00:00:00')
 
   # Time to start challengers
   TIME_TO_START_CHALLENGES = PROGRAMM_STATUS == 'test' ? Time.now - 1.day : Time.zone.parse('2018-02-18 00:00:00')
