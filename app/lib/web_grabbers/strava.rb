1629c9b0f556a19e17535e6d369765d9f9a71b13
Marino Bonetti <marinobonetti@gmail.com>
2018-01-14 22:09:41 +0100

    comparazione tra valori

--- app/lib/web_grabbers/strava.rb
+++ app/lib/web_grabbers/strava.rb
@@ -5,11 +5,11 @@ module WebGrabbers
 
       distance = web_page.css(".inline-stats .distance strong").first.text.gsub("\n",'')
       puts distance
-      self.web_distance = Unit.new(distance).convert_to('km')
+      self.web_distance = Unit.new(distance).convert_to('km').scalar
 
       elevation = web_page.css(".inline-stats .elevation strong").first.text.gsub("\n",'').gsub(",",'')
       puts elevation
-      self.web_positive_gain = Unit.new(elevation).convert_to('m')
+      self.web_positive_gain = Unit.new(elevation).convert_to('m').scalar
 
       pace = web_page.css(".inline-stats .avg-speed strong").children.first.text.gsub("\n",'')
 
