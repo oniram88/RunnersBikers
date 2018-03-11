ddf8920c748c4dc568b6f85884858b286981731a
Marino Bonetti <marinobonetti@gmail.com>
2018-03-11 22:26:09 +0100

    implementazione della ranking type

--- app/graphql/types/ranking_type.rb
+++ app/graphql/types/ranking_type.rb
@@ -1,9 +1,28 @@
 Types::RankingType = GraphQL::ObjectType.define do
   name "Ranking"
   field :id, types.Int
-  field :total_distance, types.Int
-  field :total_positive_gain, types.Int
-  field :rank, types.Int
+  field :total_distance, types.Int, "Total Run distance for user"
+  field :total_positive_gain, types.Int, "Total Positive Gain run for user"
+  field :rank, types.Int, "Ranking in the challenger"
   field :username, types.String
-  field :total_points, types.Int
+  field :total_points, types.Int, "Total points in the match"
+
+  field :challenged, types.Boolean, "If the user is inside a challenge" do
+    resolve ->(obj, args, ctx) {
+      obj.open_matches.length > 0
+    }
+  end
+
+  field :machable, types.Boolean, "If the user can be matched" do
+    resolve ->(obj, args, ctx) {
+      ctx[:current_user].machable(obj)
+    }
+  end
+
+  field :max_lose_points, types.Int, "Max points to loose in a match" do
+    resolve ->(obj, args, ctx) {
+      obj.max_lose_points(ctx[:current_user])
+    }
+  end
+
 end
