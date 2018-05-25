94bb458df36768671bd01bede47479a3e2e2146c
Marino Bonetti <marinobonetti@gmail.com>
2018-05-25 18:58:13 +0200

    inserimento limite date

--- config/initializers/runners_bikers.rb
+++ config/initializers/runners_bikers.rb
@@ -8,16 +8,16 @@ module RunnersBikers
   MATCH_DURATION = PROGRAMM_STATUS == 'test' ? 10.minutes : 5.days
 
   # Tempo inizio inserimento Performance
-  MIN_PERFORMANCE_INSERT = PROGRAMM_STATUS == 'test' ? Time.now - 1.month : Time.zone.parse('2018-02-04 00:00:00')
+  MIN_PERFORMANCE_INSERT = PROGRAMM_STATUS == 'test' ? Time.now - 1.month : Time.zone.parse('2018-06-16 00:00:00')
 
   # Tempo massimo inserimento Performance prima della fine della gara
-  MAX_PERFORMANCE_INSERT = PROGRAMM_STATUS == 'test' ? Time.now + 1.month : Time.zone.parse('2018-03-05 00:00:00') + 2.hours
+  MAX_PERFORMANCE_INSERT = PROGRAMM_STATUS == 'test' ? Time.now + 1.month : Time.zone.parse('2018-07-23 00:00:00') + 2.hours
 
   # Tempo massimo iscrizioni
   MAX_ISCRIZIONE = PROGRAMM_STATUS == 'test' ? Time.now + 1.year : MIN_PERFORMANCE_INSERT #Time.zone.parse('2018-01-13 00:00:00')
 
   # Time to start challengers
-  TIME_TO_START_CHALLENGES = PROGRAMM_STATUS == 'test' ? Time.now - 1.day : Time.zone.parse('2018-02-18 00:00:00')
+  TIME_TO_START_CHALLENGES = PROGRAMM_STATUS == 'test' ? Time.now - 1.day : Time.zone.parse('2018-06-30 00:00:00')
 
   # Tempo massimo Inizio sfide
   MAX_TIME_FOR_START_CHALLENGES =  MAX_PERFORMANCE_INSERT - MATCH_DURATION - 2.hours
