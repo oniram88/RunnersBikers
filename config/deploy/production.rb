9332c39453221aa19b1531557ea35fd95426ab81
Marino Bonetti <marinobonetti@gmail.com>
2018-05-26 14:26:47 +0200

    commentato vecchio deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,11 +3,9 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "runnersbikers.production.cloud", user: "marinobonetti", roles: %w{app db web}
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
+# server "runnersbikers.production.cloud", user: "marinobonetti", roles: %w{app db web}
 
-set :disallow_pushing, true
+# set :disallow_pushing, true
 
 # role-based syntax
 # ==================
